711d67306bb5d11a1018e09fb33a2249c81355a8
BatBrain <john.ferguson3.141596@gmail.com>
2016-10-13 19:53:01 +0000

    Completed excercise

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Sol", last_name: "Ferguson", hourly_rate: 60)
+@store1.employees.create(first_name: "Adam", last_name: "Amey", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Timmy", last_name: "Johnson", hourly_rate: 60)
+@store2.employees.create(first_name: "Justin", last_name: "Hendericks", hourly_rate: 60)
+@store2.employees.create(first_name: "David", last_name: "Coley", hourly_rate: 60)
